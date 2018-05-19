9335a9398cd3f4c8a41099caaddf1ff4992151e6
jmbotero <jbotero@hotmail.com>
2018-05-19 16:14:41 -0400

    Add comman line argument to run problem by id

--- EulerProjectSolver/Program.cs
+++ EulerProjectSolver/Program.cs
@@ -9,17 +9,24 @@ namespace JuanMartin.EulerProject
         static void Main(string[] args)
         {
             var problems = UtilityEulerProjectSolver.problems;
-            var onlyRunProblems = Convert.ToBoolean(args[0]);
+            var validateProblems = Convert.ToBoolean(args[0]);
+            var selectedPId = -1;
+            if (args.Length > 0)
+                selectedPId = Convert.ToInt32(args[1]);
 
-            if (onlyRunProblems)
+            if (selectedPId==-1)
             {
                 for (int i = 0; i < problems.Length; i++)
                 {
-                    var p = problems[i];
-                    UtilityEulerProjectSolver.Launch(p.Script, p);
-                }
+                 }
             }
             else
+            {
+                var p = UtilityEulerProjectSolver.GetProblemById(selectedPId);
+                UtilityEulerProjectSolver.Launch(p.Script, p);
+            }
+
+            if (validateProblems)
             {
                 Console.WriteLine("Verifying problem anwers...");
                 UtilityEulerProjectSolver.ValidateProblems(problems);
